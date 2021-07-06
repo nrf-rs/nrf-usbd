723863392cc1ec2bdf235c4916390d754e43d8f4
Jonas Schievink <jonasschievink@gmail.com>
2021-07-06 19:24:06 +0200

    Don't require `UsbPeripheral` to be `Sync`

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,7 @@ pub use usbd::Usbd;
 /// A trait for device-specific USB peripherals. Implement this to add support for a new hardware
 /// platform. Peripherals that have this trait must have the same register block as NRF52 USBD
 /// peripherals.
-pub unsafe trait UsbPeripheral: Send + Sync {
+pub unsafe trait UsbPeripheral: Send {
     /// Pointer to the register block
     const REGISTERS: *const ();
 }
